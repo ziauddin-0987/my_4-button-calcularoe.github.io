6d27b52fdca128e086c386c5cbf043a782c0de69
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-18 14:10:53 +0500

    this is my 4 button calculator

---
+++ index.js
@@ -0,0 +1,24 @@
+function Add() {
+  const inputt = document.getElementById("number").value;
+  const inputtt = document.getElementById("numberoo").value;
+  const max = parseInt(inputtt) + parseInt(inputt);
+  console.log(max);
+}
+function Sub() {
+  const subb = document.getElementById("number").value;
+  const subbb = document.getElementById("numberoo").value;
+  const max = parseInt(subb) - parseInt(subbb);
+  console.log(max);
+}
+function Multi() {
+  const mulll = document.getElementById("number").value;
+  const mull = document.getElementById("numberoo").value;
+  const max = parseInt(mulll) * parseInt(mull);
+  console.log(max);
+}
+function divi() {
+  const divv = document.getElementById("number").value;
+  const divvv = document.getElementById("numberoo").value;
+  const max = parseInt(divv) / parseInt(divvv);
+  console.log(max);
+}
